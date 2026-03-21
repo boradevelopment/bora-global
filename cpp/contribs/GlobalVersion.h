9b978641393f00528b2894f56aa6701fbb0ecc82
Munashe Dirwayi <me@wanomani.ac>
2026-03-21 22:11:38 +0200

    Update global

---
+++ cpp/contribs/GlobalVersion.h
@@ -0,0 +1,15 @@
+// This header is apart of the BORA Source
+// Check LICENSE.md for more information regarding the BORA license.
+
+/* 
+ * FileName: Versioning.h
+ * Purpose: Global Versioning
+ */
+#pragma once
+constexpr const char* BORA_VERSION = "0.2.5";
+// The major version integer for overhaul updates
+constexpr int BORA_VERSION_MAJOR = 0;
+// The minor version integer for big updates but not enough to change systems
+constexpr int BORA_VERSION_MINOR = 2;
+// The patch version integer for small fixes and updates
+constexpr int BORA_VERSION_PATCH = 5;
